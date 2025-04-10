44171d47c6e676b5e8713e42bccf519a90b88fb5
Hazem <hazemyasser911@gmail.com>
2025-04-10 22:06:27 +0200

    Providing Loader

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,26 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import Templates from "./pages/Templates";
-import Blogs from "./pages/Blogs";
+import { Suspense, lazy } from "react";
+
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
-import TemplateDetails from "./pages/TemplateDetails";
 import ScrollToTop from "./components/ScrollToTop";
-import ContactAffiliates from "./pages/ContactAffiliates";
-import Affiliates from "./pages/Affiliates";
-import AboutMe from "./pages/AboutMe";
-import BlogDetails from "./pages/BlogDetails";
-import PrivacyPolicyPage from "./components/PrivacyPolicyPage";
-import TermsAndConditionsPage from "./components/TermsAndConditionsPage";
-import NotFound from "./components/NotFound";
-import ServerError from "./components/ServerError";
+import Loader from "./components/Loader";
+
+// Lazy loaded pages
+const Home = lazy(() => import("./pages/Home"));
+const Templates = lazy(() => import("./pages/Templates"));
+const TemplateDetails = lazy(() => import("./pages/TemplateDetails"));
+const Blogs = lazy(() => import("./pages/Blogs"));
+const BlogDetails = lazy(() => import("./pages/BlogDetails"));
+const ContactAffiliates = lazy(() => import("./pages/ContactAffiliates"));
+const Affiliates = lazy(() => import("./pages/Affiliates"));
+const AboutMe = lazy(() => import("./pages/AboutMe"));
+const PrivacyPolicyPage = lazy(() => import("./components/PrivacyPolicyPage"));
+const TermsAndConditionsPage = lazy(() =>
+  import("./components/TermsAndConditionsPage")
+);
+const NotFound = lazy(() => import("./components/NotFound"));
+const ServerError = lazy(() => import("./components/ServerError"));
 
 function App() {
   return (
@@ -22,26 +29,31 @@ function App() {
         <ScrollToTop />
         <Navbar />
         <div className="flex-grow mt-16">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/templates" element={<Templates />} />
-            <Route
-              path="/templates/:templateSlug"
-              element={<TemplateDetails />}
-            />
-            <Route path="/blogs" element={<Blogs />} />
-            <Route path="/blogs/:slug" element={<BlogDetails />} />
-            <Route path="/contact-affiliates" element={<ContactAffiliates />} />
-            <Route path="/about-me" element={<AboutMe />} />
-            <Route path="/affiliates" element={<Affiliates />} />
-            <Route path="/privacy-policy" element={<PrivacyPolicyPage />} />
-            <Route
-              path="/terms-and-conditions"
-              element={<TermsAndConditionsPage />}
-            />
-            <Route path="*" element={<NotFound />} />
-            <Route path="/500" element={<ServerError />} />
-          </Routes>
+          <Suspense fallback={<Loader />}>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/templates" element={<Templates />} />
+              <Route
+                path="/templates/:templateSlug"
+                element={<TemplateDetails />}
+              />
+              <Route path="/blogs" element={<Blogs />} />
+              <Route path="/blogs/:slug" element={<BlogDetails />} />
+              <Route
+                path="/contact-affiliates"
+                element={<ContactAffiliates />}
+              />
+              <Route path="/about-me" element={<AboutMe />} />
+              <Route path="/affiliates" element={<Affiliates />} />
+              <Route path="/privacy-policy" element={<PrivacyPolicyPage />} />
+              <Route
+                path="/terms-and-conditions"
+                element={<TermsAndConditionsPage />}
+              />
+              <Route path="/500" element={<ServerError />} />
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+          </Suspense>
         </div>
         <Footer />
       </Router>
